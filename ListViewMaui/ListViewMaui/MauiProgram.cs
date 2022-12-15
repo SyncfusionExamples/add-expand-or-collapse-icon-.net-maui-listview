82e7ab42563e7e5453f31ba7f540917cf7040aa5
Suthi Yuvaraj <suthi.yuvaraj@syncfusion.com>
2022-12-15 19:42:14 +0530

    MAUI-3771 : Samples updated

--- ListViewMaui/ListViewMaui/MauiProgram.cs
+++ ListViewMaui/ListViewMaui/MauiProgram.cs
@@ -12,8 +12,8 @@ public static class MauiProgram
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-
 		builder.ConfigureSyncfusionListView();
 		return builder.Build();
 	}
